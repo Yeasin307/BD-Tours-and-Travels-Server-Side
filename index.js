b21889dc9664e5649b34cef87f334508796f81f6
Yeasin <yeasinhossen7972@gmail.com>
2021-11-03 01:04:13 +0600

    final

--- index.js
+++ index.js
@@ -1,12 +1,84 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
 
-const port = 5000;
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middle Ware
+app.use(cors());
+app.use(express.json());
+
+/* const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true }); */
+
+
+const uri = "mongodb+srv://mongodbuser01:hNWnX50luk1JzgMr@cluster0.lcr1a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('BD-Tours-and-Travels');
+        const serviceCollection = database.collection('Services');
+        const orderCollection = database.collection('Orders');
+
+
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+        //GET Products API
+        /*  app.get('/products', async (req, res) => {
+             const cursor = productCollection.find({});
+             const page = req.query.page;
+             const size = parseInt(req.query.size);
+             let products;
+             const count = await cursor.count();
+ 
+             if (page) {
+                 products = await cursor.skip(page * size).limit(size).toArray();
+             }
+             else {
+                 products = await cursor.toArray();
+             }
+ 
+             res.send({
+                 count,
+                 products
+             });
+         });
+ 
+         // Use POST to get data by keys
+         app.post('/products/byKeys', async (req, res) => {
+             const keys = req.body;
+             const query = { key: { $in: keys } }
+             const products = await productCollection.find(query).toArray();
+             res.send(products);
+         });
+ 
+         // Add Orders API
+         app.post('/orders', async (req, res) => {
+             const order = req.body;
+             const result = await orderCollection.insertOne(order);
+             res.json(result);
+         }) */
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Welcome BD Tours and Travels');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log('Server is running at port', port);
 })
