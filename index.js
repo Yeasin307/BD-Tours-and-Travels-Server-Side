b80c64c50563316d2a247caf0f1df663c24e366f
Yeasin <yeasinhossen7972@gmail.com>
2021-11-04 00:08:15 +0600

    final

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
-require('dotenv').config();
 const cors = require('cors');
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,12 +11,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-/* const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true }); */
 
-
-const uri = "mongodb+srv://mongodbuser01:hNWnX50luk1JzgMr@cluster0.lcr1a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lcr1a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 async function run() {
     try {
@@ -31,41 +30,28 @@ async function run() {
             res.send(users);
         })
 
-        //GET Products API
-        /*  app.get('/products', async (req, res) => {
-             const cursor = productCollection.find({});
-             const page = req.query.page;
-             const size = parseInt(req.query.size);
-             let products;
-             const count = await cursor.count();
- 
-             if (page) {
-                 products = await cursor.skip(page * size).limit(size).toArray();
-             }
-             else {
-                 products = await cursor.toArray();
-             }
- 
-             res.send({
-                 count,
-                 products
-             });
-         });
- 
-         // Use POST to get data by keys
-         app.post('/products/byKeys', async (req, res) => {
-             const keys = req.body;
-             const query = { key: { $in: keys } }
-             const products = await productCollection.find(query).toArray();
-             res.send(products);
-         });
- 
-         // Add Orders API
-         app.post('/orders', async (req, res) => {
-             const order = req.body;
-             const result = await orderCollection.insertOne(order);
-             res.json(result);
-         }) */
+
+        app.get('/services/:_id', async (req, res) => {
+            const id = req.params._id;
+            const query = { _id: ObjectId(id) };
+            const user = await serviceCollection.findOne(query);
+            console.log('load user with id:', id);
+            res.send(user);
+        })
+
+
+        app.get('/orders', async (req, res) => {
+            const cursor = orderCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+
+        app.post('/orders', async (req, res) => {
+            const newOrder = req.body;
+            const result = await orderCollection.insertOne(newOrder);
+            res.json(result);
+        })
 
     }
     finally {
